ca4cc6a9987f902506b787c560001056391c35d5
MinuraWije <minurawije@gmail.com>
2024-05-09 10:28:36 +0530

    updated the navigation.

---
+++ js/spa.js
@@ -0,0 +1,22 @@
+$('#nav-customer').on('click',() =>  {
+    $('#customer-section').css({display: 'block'});
+    $('#item-section').css({display: 'none'});
+    $('#order-section').css({display: 'none'});
+});
+$('#nav-item').on('click',() =>  {
+    $('#customer-section').css({display: 'none'});
+    $('#item-section').css({display: 'block'});
+    $('#order-section').css({display: 'none'});
+});
+
+$('#nav-order').on('click',() =>  {
+    $('#customer-section').css({display: 'none'});
+    $('#item-section').css({display: 'none'});
+    $('#order-section').css({display: 'block'});
+});
+
+$('#nav-home').on('click',() =>  {
+    $('#customer-section').css({display: 'block'});
+    $('#item-section').css({display: 'block'});
+    $('#order-section').css({display: 'block'});
+});
