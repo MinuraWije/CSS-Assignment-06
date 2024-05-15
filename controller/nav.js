e09c3909eea529ea5f769e4e52a414b27d045ba0
MinuraWije <minurawije@gmail.com>
2024-05-15 10:25:05 +0530

    completed item model

--- controller/nav.js
+++ controller/nav.js
@@ -1,3 +1,9 @@
+$('#nav-home').on('click',() =>  {
+    $('#customer-section').css({display: 'none'});
+    $('#item-section').css({display: 'none'});
+    $('#order-section').css({display: 'none'});
+});
+
 $('#nav-customer').on('click',() =>  {
     $('#customer-section').css({display: 'block'});
     $('#item-section').css({display: 'none'});
@@ -15,8 +21,3 @@ $('#nav-order').on('click',() =>  {
     $('#order-section').css({display: 'block'});
 });
 
-$('#nav-home').on('click',() =>  {
-    $('#customer-section').css({display: 'none'});
-    $('#item-section').css({display: 'none'});
-    $('#order-section').css({display: 'none'});
-});
